ff4ee28035d9678047c8ed90b62bf55610e68c12
Hjalmar Thunberg <hjalmar.thunberg@gmail.com>
2021-02-04 13:05:22 +0100

    assigmnent 2 exercice 6
    simple que test program

---
+++ workpackage_2/exerc_2_6.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 5          // Define max queue size
+#define EMPTY_SPACE -1 //Define empty queue space
+
+// TODO: FIX so it shows -1 instead of bs
+// Initialize an empty queue with EMPTY_SPACE
+void initQue(int list[], int max)
+{
+    for (int i = 0; i < max; i++)
+    {
+        list[i] = EMPTY_SPACE;
+    }
+}
+
+// Input an item into the queue
+int input(int list[], int number, int max) //queue
+{
+    printf("Queuing number: %d\n", number);
+    // Iterate through queue until empty space is reached
+    for (int i = 0; i < max; i++)
+    {
+        // Replace empty space with number
+        if (list[i] == EMPTY_SPACE)
+        {
+            list[i] = number;
+            printf("Number %d was queued.\n", number);
+            return 1;
+        }
+    }
+    printf("Queue is full\n");
+    return 0;
+}
+// "dequeues" the first element of the array if the element is not equal to EMPTY_SPACE (-1).
+int output(int list[], int max)
+{
+    // Iterate through queue if first element is not empty print element
+    for (int i = 0; i < max; i++)
+    {
+        if (list[i] != EMPTY_SPACE)
+        {
+            printf("Dequeued %d.\n", list[i]);
+            // Scooch all other elements down
+            for (int j = 1; j < max; j++)
+            {
+                list[j - 1] = list[j];
+                list[j] = EMPTY_SPACE;
+            }
+            return list[0];
+        }
+        return 0;
+    }
+}
+// init queue and run sequence of methods to test
+int main(void)
+{
+    int list[MAX];
+    initQue(list, MAX); // Init new queue
+    for (int i = 0; i < MAX; i++) {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+    input(list, 3, MAX);
+    input(list, 4, MAX);
+    input(list, 5, MAX);
+    input(list, 6, MAX);
+    input(list, 7, MAX);
+    input(list, 8, MAX);
+    for (int i = 0; i < MAX; i++) {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+    output(list, MAX);
+    output(list, MAX);
+    for (int i = 0; i < MAX; i++) {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+    input(list, 11, MAX);
+    for (int i = 0; i < MAX; i++) {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+}
