79b2e9b5e60090fdfee89681f23e48e2bd2cd569
Hjalmar Thunberg <hjalmar.thunberg@gmail.com>
2021-02-04 13:00:47 +0100

    assignment 2 exercice 1
    simple program to copy strings (including textfile as terminal argument)

---
+++ workpackage_2/exerc_2_1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 20 // Max length of input text
+
+void copyString(char *location, char *toCopy)
+{
+    // Iterate through until the end of the string
+    while (*toCopy != '\0') { 
+        // Assign the pointer for the target the value of the string to copy
+        // then increment both pointers
+        *location++ = *toCopy++; 
+        
+    }                           
+}
+
+int main(int argc, char *argv[])
+{
+    char s_copyLib[MAX]; // Init the 'copy with library' array
+    char s_copyFunc[MAX]; // Init the 'copy with our function' array
+    char s_input[MAX]; // Init the user input / file input array
+
+    // // If there is no extra arguments (file redirect)
+    // if (argc == 1) 
+    // {
+    //     printf(">> ");
+    //     gets(s_input);
+    //     s_input[MAX] = '\0'; // Clip the user input to max 20 chars
+    //     // Call lib copy
+    //     strcpy(s_copyLib, s_input); 
+    //     // Call our copy
+    //     copyString(s_copyFunc, s_input); 
+    // }
+    // // If there is more than one arg then we get the file redirect
+    // else 
+    // {
+    //     strcpy(s_copyLib, argv[1]);
+    //     copyString(s_copyFunc, argv[1]);
+    // }
+
+    // Better code discussed during review
+    fgets(s_input, 20, stdin);
+    strcpy(s_copyLib, s_input);
+    copyString(s_copyFunc, s_input);
+
+    printf("String copied using library: %s\n", s_copyLib);
+    printf("String copied using own function: %s\n", s_copyFunc);
+    // Free up the memory
+    free(s_copyLib); 
+    free(s_copyFunc); 
+    free(s_input); 
+    return 0;
+}
